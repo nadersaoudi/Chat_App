fb53701148307dfb2935805ca9eab46641289fc2
saoudinader <saoudi.nader7@gmail.com>
2022-07-15 18:26:03 +0100

    ADD: Set Avatar Controller/Page

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -41,3 +41,21 @@ module.exports.login = async (req, res, next) => {
     next(ex);
   }
 };
+
+//Set Avatar Picture
+module.exports.setAvatar = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const avatarImage = req.body.image;
+    const userData = await User.findByIdAndUpdate(userId, {
+      isAvatarImageSet: true,
+      avatarImage,
+    });
+    return res.json({
+      isSet: userData.isAvatarImageSet,
+      image: userData.avatarImage,
+    });
+  } catch (ex) {
+    next(ex);
+  }
+};
